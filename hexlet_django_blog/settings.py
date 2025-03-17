237afd5b22a150b89074e997d03898faa17261ac
toro <timofey89rus@gmail.com>
2025-03-17 11:32:41 +0300

    add articles app

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "hexlet_django_blog"
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "hexlet_django_blog",
+    "hexlet_django_blog.article",
 ]
 
 MIDDLEWARE = [
