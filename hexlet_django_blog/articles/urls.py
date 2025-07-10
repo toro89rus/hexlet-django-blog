bf60c4b969d5789feb9891f7a24aa0c172a1e341
toro <timofey89rus@gmail.com>
2025-07-10 11:26:30 +0300

    add create page

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from hexlet_django_blog.articles.views import IndexView, ArticleView
+from hexlet_django_blog.articles.views import (
+    ArticleView,
+    ArticleFormCreateView,
+    IndexView,
+)
 
 app_name = "articles"
 
 urlpatterns = [
-    path("", IndexView.as_view()),
+    path("", IndexView.as_view(), name="articles_index"),
     path("<int:pk>/", ArticleView.as_view(), name="article_detail"),
+    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
 ]
--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from hexlet_django_blog.articles.models import Article
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -14,3 +15,10 @@ class ArticleView(DetailView):
     model = Article
     template_name = "articles/show.html"
     context_object_name = "article"
+
+
+class ArticleFormCreateView(CreateView):
+    model = Article
+    fields = ["name", "body"]
+    template_name = "articles/create.html"
+    success_url = reverse_lazy("articles:articles_index")
